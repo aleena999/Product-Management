c832bd34e3f1f3cebc39f92a189644f9fc0077a1
aleena999 <69433694+aleena999@users.noreply.github.com>
2022-10-10 09:46:05 +0530

    Add files via upload

---
+++ server/routes/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors'
+
+import Routes from './routes/route.js';
+import Connection from './database/db.js';
+
+const app = express();
+
+dotenv.config();
+
+
+app.use(express.json());
+
+app.use(bodyParser.json({
+    extended: true
+}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cors());
+
+app.use('/', Routes);
+
+const PORT = process.env.PORT | 5000;
+
+Connection();
+
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
